e0df6896047a23b37a91d0c880a2c3a9de9ef0b1
Hossam-Sayed <hossamsayed3030@gmail.com>
2022-05-18 23:22:53 +0200

    Added first version of Cooking and Pause

---
+++ EmbeddedProject/Cooking&Pause.c
@@ -0,0 +1,34 @@
+
+int pause() {
+	while(1){
+		int i;
+		for(i = 0; i < 100; i++){
+			/*
+			if(sw2 is pressed && door is closed){
+					return 1;
+			}
+			if (sw1 is pressed){
+				return 0;
+			}*/
+			// wait 10ms
+		}
+		// Toggle the LEDs
+	}
+}
+
+int cooking(int delay){
+	int i;
+	// turn on LEDs
+	for(i = 0; i < delay * 100; i++){
+		/*
+		if (sw1 is pressed || door is open){
+			while(sw1 is pressed);
+			if (!pause()){
+				//clear LCD
+				return 0;
+			}
+		}*/
+		// wait 10ms
+	}
+	return 1;
+}
