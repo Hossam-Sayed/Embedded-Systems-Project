40e355eb4ba30fcf9375379239616a35dba8bb1b
Hossam Sayed Nasr <83096913+Hossam-Sayed@users.noreply.github.com>
2022-05-22 16:51:45 +0200

    Added start_cooking and finished_cooking functions

--- EmbeddedProject/Cooking.c
+++ EmbeddedProject/Cooking.c
@@ -24,11 +24,36 @@ _Bool cooking(int delay){
 		if (SW1() || !checkingDoor()){
 			while(SW1());
 			if (!pause()){
-				//clear LCD
+				// clear LCD
 				return false;
 			}
 		}
 		// wait 10ms
 	}
 	return true;
+}
+
+void start_cooking(int cooking_time) {
+	if(cooking(cooking_time)){
+		FinishedCooking();
+	}
+	else {
+		// clear LCD
+	}
+}
+
+void finished_cooking(){
+	int i;
+	// clear LCD
+	// print "Finished Cooking" on LCD
+	// turn ON Buzzer
+	// turn ON LEDs
+	for(i = 0; i < 6; i++)
+	{
+		// toggle LEDs
+		// toggle Buzzer
+		// wait 1s
+	}
+	// turn OFF LEDs
+	// turn OFF Buzzer
 }
