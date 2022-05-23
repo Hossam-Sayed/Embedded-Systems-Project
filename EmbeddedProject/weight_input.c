8fd1ab5f7233b311d5e9448b502ec9fc9bcc5287
Mustafa-elite <90336988+Mustafa-elite@users.noreply.github.com>
2022-05-23 17:59:16 +0200

    Update weight_input.c

--- EmbeddedProject/weight_input.c
+++ EmbeddedProject/weight_input.c
@@ -1,19 +1,22 @@
-int weight_input()
+int weight_input(char s)
 {
     int weight;
     while(1)
     {
-        //print_on_LCD("chicken weight?");
-        weight=/*keypad_input()*/-'0';
-        if(weight>=1 |weight<=9)
-            {
-                return weight;
-            }
-        else
-        {
-            //print_on_LCD("Err");
-            delay_ms(2000);
-        }
-
+			weight = keypad_input() - '0';
+			if(weight < 1 || weight > 9)
+			{
+				printLCD("Err");
+				delay_ms(2000);
+				if(s=='c')
+					printLCD("Chicken Weight?");
+				else
+					printLCD("Beef Weight?");
+			}
+			else
+			{
+				return weight;
+			}
+	
     }
 }
