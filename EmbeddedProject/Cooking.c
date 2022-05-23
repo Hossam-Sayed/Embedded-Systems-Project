9c711d7eca5c384e667076de2e1a0a8894c8c0b2
Hossam Sayed Nasr <83096913+Hossam-Sayed@users.noreply.github.com>
2022-05-23 10:57:48 +0200

    Added counter function and modified the code

--- EmbeddedProject/Cooking.c
+++ EmbeddedProject/Cooking.c
@@ -4,31 +4,35 @@ _Bool pause() {
 		int i;
 		for(i = 0; i < 100; i++){
 			if(SW2() && checkingDoor()){
-				// turn ON LEDs
+				GPIO_PORTF_DATA_R |= 0x0E; // turn ON LEDs
 				return true;
 			}
 			if (SW1()){
-				// turn OFF LEDs
+				GPIO_PORTF_DATA_R &= ~0x0E; // turn OFF LEDs
 				return false;
 			}
-			// wait 10ms
+			delay_ms(10); // wait 10ms
 		}
-		// Toggle the LEDs
+		GPIO_PORTF_DATA_R ^= 0x0E; // Toggle the LEDs
 	}
 }
 
 _Bool cooking(int delay){
 	int i;
-	// turn ON LEDs
-	for(i = 0; i < delay * 100; i++){
-		if (SW1() || !checkingDoor()){
-			while(SW1());
-			if (!pause()){
-				// clear LCD
-				return false;
+	int j;
+	GPIO_PORTF_DATA_R |= 0x0E; // turn ON LEDs
+	for(j = 0; j < delay; j++){
+		counter(delay - j);
+		for(i = 0; i < 100; i++){
+			if (SW1() || !checkingDoor()){
+				while(SW1());
+				if (!pause()){
+					LCD_command(1); // clear LCD
+					return false;
+				}
 			}
+			delay_ms(10); // wait 10ms
 		}
-		// wait 10ms
 	}
 	return true;
 }
@@ -38,22 +42,33 @@ void start_cooking(int cooking_time) {
 		FinishedCooking();
 	}
 	else {
-		// clear LCD
+		LCD_command(1); // clear LCD
 	}
 }
 
 void finished_cooking(){
 	int i;
-	// clear LCD
-	// print "Finished Cooking" on LCD
-	// turn ON Buzzer
-	// turn ON LEDs
+	LCD_command(1); // clear LCD
+	printLCD("Finished Cooking"); // print "Finished Cooking" on LCD
+	GPIO_PORTE_DATA_R |= 0x20; // turn ON Buzzer
+	GPIO_PORTF_DATA_R |= 0x0E; // turn ON LEDs
 	for(i = 0; i < 6; i++)
 	{
-		// toggle LEDs
-		// toggle Buzzer
-		// wait 1s
+		GPIO_PORTF_DATA_R ^= 0x0E; // toggle LEDs
+		GPIO_PORTE_DATA_R ^= 0x20; // toggle Buzzer
+		delay_ms(1000); // wait 1s
 	}
-	// turn OFF LEDs
-	// turn OFF Buzzer
+	GPIO_PORTF_DATA_R &= ~0x0E; // turn OFF LEDs
+	GPIO_PORTE_DATA_R &= ~0x20; // turn OFF Buzzer
+}
+
+void counter(int seconds){
+	int sec;
+	int minute;
+	char c[5];  
+	LCD_command(1); // clear LCD
+	minute =(int)(seconds/60) ;
+	sec = seconds - 60 * minute ;
+	sprintf(c,"%02d:%02d",minute, sec);
+	printLCD(c); // print current time on LCD
 }
